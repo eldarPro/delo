d5874b1a53bf4aaa1f51b4f6c0b98fa7951f47c2
Eldar <eldar@telega.in>
2024-04-11 06:58:15 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,4 @@ import store from './store';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<Provider store={store}><App /></Provider>);
+root.render(<App />);
